13bc80e5c98751821c75cf8f312d152e9b1e1363
Julien Bouquillon <julien@bouquillon.com>
2015-07-10 03:35:07 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# node-timecodes ![npm](https://img.shields.io/npm/v/node-timecodes.svg) ![license](https://img.shields.io/npm/l/node-timecodes.svg) ![github-issues](https://img.shields.io/github/issues/revolunet/node-timecodes.svg)
+
+Time and SMTPE Timecodes conversions
+
+https://documentation.apple.com/en/cinematools/usermanual/index.html#chapter=2%26section=5%26tasks=true
+
+https://en.wikipedia.org/wiki/SMPTE_timecode
+
+![nodei.co](https://nodei.co/npm/node-timecodes.png?downloads=true&downloadRank=true&stars=true)
+
+## Features
+
+
+## Scripts
+
+ - **npm run readme** : `node ./node_modules/node-readme/.bin/node-readme.js`
+ - **npm run test** : `./node_modules/babel-tape-runner/bin/babel-tape-runner spec/**/*.spec.js | ./node_modules/.bin/tap-spec`
+ - **npm run build** : `babel -d ./dist ./src`
+
+
+## Author
+
+Julien Bouquillon <julien@bouquillon.com> http://github.com/revolunet
+
+## License
+
+ - **MIT** : https://spdx.org/licenses/MIT.html
---
+++ dist/index.js
@@ -0,0 +1,54 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+    value: true
+});
+/* simple timecode<->time conversion */
+
+var TIMECODE_REGEXP = /\d+\:\d+\:\d+\:\d+/;
+var defaultFramerate = 25;
+
+function padNumber(nb) {
+    if (nb < 10) {
+        nb = '0' + nb;
+    }
+    return nb;
+}
+
+exports['default'] = {
+
+    /** converts time to timecode */
+    toTimecode: function toTimecode(seconds) {
+        var frameRate = arguments.length <= 1 || arguments[1] === undefined ? defaultFramerate : arguments[1];
+
+        if (isNaN(seconds)) {
+            throw new Error('seconds should be a number');
+        }
+
+        var seconds = parseFloat(seconds),
+            ms = Math.round((seconds - parseInt(seconds, 10)) * 10000, 10) / 10000;
+
+        var hours = parseInt(seconds / (60 * 60), 10),
+            mins = parseInt(seconds / 60, 10) - hours * 60,
+            secs = parseInt(seconds - hours * 3600 - mins * 60, 10),
+            frame = Math.floor(Math.round(ms / (1 / frameRate) * 100) / 100);
+
+        return padNumber(hours) + ':' + padNumber(mins) + ':' + padNumber(secs) + ':' + padNumber(frame);
+    },
+
+    /* converts timecode to time */
+    toTime: function toTime(timecode) {
+        var frameRate = arguments.length <= 1 || arguments[1] === undefined ? defaultFramerate : arguments[1];
+
+        if (!TIMECODE_REGEXP.test(timecode)) {
+            throw new Error('invalid timecode : ' + timecode);
+        }
+        if (typeof timecode === 'number') {
+            return timecode;
+        }
+        var tc = timecode.split(':');
+
+        return parseFloat(tc[0] * 60 * 60 + parseInt(tc[1], 10) * 60 + parseInt(tc[2], 10) + parseInt(tc[3], 10) / frameRate);
+    }
+};
+module.exports = exports['default'];
