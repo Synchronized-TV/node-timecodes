855263929f843a1f3a122e737c45319a7895332c
Julien Bouquillon <julien@bouquillon.com>
2016-04-14 21:44:03 +0200

    update dist

--- dist/fromSeconds.js
+++ dist/fromSeconds.js
@@ -7,7 +7,9 @@ Object.defineProperty(exports, '__esModule', {
 var _constants = require('./constants');
 
 function padNumber(nb) {
-  if (nb < 10) {
+  var length = arguments.length <= 1 || arguments[1] === undefined ? 2 : arguments[1];
+
+  while (('' + nb).length < length) {
     nb = '0' + nb;
   }
   return nb;
@@ -15,21 +17,28 @@ function padNumber(nb) {
 
 /* converts time to timecode */
 function fromSeconds(seconds) {
-  var frameRate = arguments.length <= 1 || arguments[1] === undefined ? _constants.defaultFramerate : arguments[1];
+  var _ref = arguments.length <= 1 || arguments[1] === undefined ? {} : arguments[1];
+
+  var _ref$frameRate = _ref.frameRate;
+  var frameRate = _ref$frameRate === undefined ? _constants.defaultFramerate : _ref$frameRate;
+  var _ref$ms = _ref.ms;
+  var ms = _ref$ms === undefined ? false : _ref$ms;
 
   if (isNaN(seconds)) {
     throw new Error('seconds should be a number');
   }
 
   var _seconds = parseFloat(seconds),
-      ms = Math.round((_seconds - parseInt(_seconds, 10)) * 10000, 10) / 10000;
+      milliseconds = Math.round((_seconds - parseInt(_seconds, 10)) * 10000, 10) / 10;
 
   var hours = Math.floor(_seconds / (60 * 60), 10),
       mins = Math.floor(_seconds / 60, 10) - hours * 60,
       secs = Math.floor(_seconds - hours * 3600 - mins * 60, 10),
-      frame = Math.floor(Math.round(ms / (1 / frameRate) * 100) / 100);
+      frame = Math.floor(Math.round(milliseconds / 1000 / (1 / frameRate) * 100) / 100);
 
-  return padNumber(hours) + ':' + padNumber(mins) + ':' + padNumber(secs) + ':' + padNumber(frame);
+  var suffix = ms && padNumber(parseInt(milliseconds, 10), 3) || padNumber(frame, 2);
+
+  return padNumber(hours) + ':' + padNumber(mins) + ':' + padNumber(secs) + ':' + suffix;
 }
 
 exports['default'] = fromSeconds;
